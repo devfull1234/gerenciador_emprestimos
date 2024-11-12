a683e4b0524f5c583aa0d5b24117a25c328a006a
brunonick26 <brunorecord1234@gmail.com>
2024-11-12 19:38:18 -0300

    att para deploy2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignorar erros de linting durante o build
+  },
 };
 
 export default nextConfig;
